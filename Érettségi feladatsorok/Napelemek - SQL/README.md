bc1a090a8e55fd0e87cc14405754a613cd9b1b02
Bognár Pál <bognar.pal@siro.hu>
2020-04-02 23:16:11 +0200

    Napelemek SQL feladatsor

---
+++ Érettségi feladatsorok/Napelemek - SQL/README.md
@@ -0,0 +1,9 @@
+# Egyszámjáték
+2020-as Informatika ágazati szakmai minta érettségi adatbázikezelő feladat
+
+## Feladat leírás és kiindó állomány
+Feladat leírása: feladat.pdf  
+Induló állományok: adatbazis.sql
+
+## Kapcsolódó videó (megoldás dbForge-al)
+https://youtu.be/mAb8wnyAZkI
---
+++ Érettségi feladatsorok/Napelemek - SQL/megoldasok.sql
@@ -0,0 +1,42 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+
+-- 1. feladat:
+CREATE DATABASE napsutes
+	CHARACTER SET utf8
+	COLLATE utf8_hungarian_ci;
+
+-- 3. feladat:
+update regiok
+   set regioNev = "Észak-Írország"
+where regioNev = "Észak Írország";
+
+-- 4. feladat:
+SELECT
+  COUNT(meresek.id) AS rekordszam,
+  AVG(meresek.perc) AS atlag
+FROM meresek;
+
+-- 5. feladat:
+SELECT
+  meresek.ev,
+  SUM(meresek.perc / 60) AS orak
+FROM meresek
+  INNER JOIN regiok
+    ON meresek.regioId = regiok.id
+WHERE regiok.regioNev = 'Anglia'
+AND meresek.ev BETWEEN 1990 AND 2000
+GROUP BY meresek.ev
+ORDER BY meresek.ev DESC;
+
+-- 6. feladat:
+SELECT
+  meresek.ev,
+  meresek.perc,
+  regiok.regioNev AS terulet
+FROM meresek
+  INNER JOIN regiok
+    ON meresek.regioId = regiok.id
+WHERE meresek.ho = 2
+AND meresek.perc > 6000
+ORDER BY meresek.perc DESC
