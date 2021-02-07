2ce8324d36bb52ea2be3350a25ede0f27b4b08d6
Bognár Pál <bognar.pal@siro.hu>
2021-02-07 10:02:48 +0100

    Szudoku

---
+++ Érettségi feladatsorok/Szudoku/sudokuCLI/sudokuCLI/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace sudokuCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Feladvany> feladvanyok = new List<Feladvany>();
+            StreamReader sr = new StreamReader("feladvanyok.txt");
+            while (!sr.EndOfStream)
+            {
+                feladvanyok.Add(new Feladvany(sr.ReadLine()));
+            }
+            sr.Close();
+            Console.WriteLine("3. feladat: Beolvasva {0} feladvány", feladvanyok.Count);
+
+            int meret;
+            do
+            {
+                Console.Write("4. feladat: Kérem a feladvány méretét [4..9]: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out meret) || meret < 4 || meret > 9);
+
+
+            List<Feladvany> nElemuFeladvanyok = new List<Feladvany>();
+            foreach (var f in feladvanyok)
+            {
+                if (f.Meret == meret)
+                {
+                    nElemuFeladvanyok.Add(f);
+                }
+            }
+
+            Console.WriteLine("{0}x{0} méretű feladványból {1} darab van tárolva", meret, nElemuFeladvanyok.Count);
+
+            /*
+            Random rand = new Random();
+            int sorszam = rand.Next(db);
+            int i = 0;
+            int ii = 0;
+            while ( ii < sorszam)
+            {
+                if (feladvanyok[i].Meret == meret)
+                {
+                    ii++;
+                }
+                i++;
+            }
+            var kivalasztottFeladvany = feladvanyok[i - 1];
+            */
+            Random rand = new Random();
+            int index = rand.Next(nElemuFeladvanyok.Count);
+            var kivalasztottFeladvany = nElemuFeladvanyok[index];
+
+            Console.WriteLine("5. feladat: A kiválaszott feladvány: ");
+            Console.WriteLine(kivalasztottFeladvany.Kezdo);
+
+            double db = 0;
+            foreach (char szamjegy in kivalasztottFeladvany.Kezdo)
+            {
+                if (szamjegy != '0')
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine("6. feladat: A feladvány kitöltöttsége: {0:f0}%)", 100 * db / kivalasztottFeladvany.Kezdo.Length);
+
+            Console.WriteLine("7. feladat: A feladvány kirajzolva:");
+            kivalasztottFeladvany.Kirajzol();
+
+            string fajlNev = string.Format("sudoku{0}.txt", meret);
+            StreamWriter sw = new StreamWriter(fajlNev);
+            foreach (var f in nElemuFeladvanyok)
+            {
+                sw.WriteLine(f.Kezdo);
+            }
+            sw.Close();
+            Console.WriteLine("8. feladat: {0} állomány {1} darab feladvánnyal létrehozva", fajlNev, nElemuFeladvanyok.Count);
+        }
+    }
+}
---
+++ Érettségi feladatsorok/Szudoku/sudokuCLI/sudokuGUI/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sudokuGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinusz_Click(object sender, RoutedEventArgs e)
+        {
+            int meret = int.Parse(txtMeret.Text);
+            if (meret > 4)
+            {
+                txtMeret.Text = (meret - 1).ToString();
+            }
+        }
+
+        private void btnPlusz_Click(object sender, RoutedEventArgs e)
+        {
+            int meret = int.Parse(txtMeret.Text);
+            if (meret < 9)
+            {
+                txtMeret.Text = (meret + 1).ToString();
+            }
+
+        }
+
+        private void txtKezdoAllapot_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblHossz.Content = string.Format("Hossz: {0}", txtKezdoAllapot.Text.Length);
+        }
+
+        private void btnEllenorzes_Click(object sender, RoutedEventArgs e)
+        {
+            int meret = int.Parse(txtMeret.Text);
+            int hossz = txtKezdoAllapot.Text.Length;
+            if (meret * meret == hossz)
+            {
+                MessageBox.Show("A feladvány megfelelő hosszúságú");
+            }
+            else if (meret * meret > hossz)
+            {
+                MessageBox.Show(string.Format("A feladvány rövid: kell még {0} számjegy", meret * meret - hossz));
+            }
+            else
+            {
+                MessageBox.Show(string.Format("A feladvány hosszú: törlendő {0} számjegy", hossz - meret * meret));
+            }
+        }
+    }
+}
