47fc2ef0b74835f76b1010f763f21719260b66a7
Bognár Pál <bognar.pal@siro.hu>
2021-02-16 16:44:11 +0100

    PDF report hozzáadása

--- Fullstack/E-Munkalap/E-Munkalap/Controllers/WorkController.cs
+++ Fullstack/E-Munkalap/E-Munkalap/Controllers/WorkController.cs
@@ -112,5 +112,138 @@ namespace E_Munkalap.Controllers
                 return Ok(true);
             });
         }
+
+        [HttpGet]
+        [Route("works/pdf/{id}")]
+        public IActionResult GetPdf(int id)
+        {
+            return this.RunWithErrorHandling(() =>
+            {
+                var work = databaseProvider.Query<Work>("work.works_select", new { Id = id }).FirstOrDefault();
+                if (work != null)
+                {
+                    return File(createReport(work), "application/pdf", $"munkalap_{work.Id}.pdf");
+                }
+                return NotFound("Nem létező azonosító!");
+            });
+        }
+
+        private byte[] createReport(Work work)
+        {
+            var html = "<html lang = \"hu\">" +
+                       "<head>" +
+                       "   <meta charset = \"utf-8\" >" +
+                       "   <style> " +
+                       "      body {" +
+                       "         font-size: 16pt;" +
+                       "      }" +
+                       "      h1 {" +
+                       "         text-align: center;" +
+                       "         font-size: 1.5rem;" +
+                       "         margin-top: 4rem;" +
+                       "         margin-bottom: 2rem;" +
+                       "      }" +
+                       "      .alcim {" +
+                       "         text-align: center;" +
+                       "         font-size: 1.2rem;" +
+                       "         font-style: italic;" +
+                       "      }" +
+                       "      fieldset {" +
+                       "          width: 50rem;" +
+                       "          margin: 0 auto;" +
+                       "          padding: 0rem 2rem;" +
+                       "      }" +
+                       "      fieldset p {" +
+                       "          font-weight: normal;" +
+                       "          padding-left: 7rem;" +
+                       "      }" +
+                       "      .label {" +
+                       "          font-weight: bold;" +
+                       "          display: inline-block;" +
+                       "          margin-left: -7rem;" +
+                       "          width: 6.75rem;" +
+                       "      }" +
+                       "      .alairas {" +
+                       "          text-align: center;" +
+                       "          margin-right: 2rem;" +
+                       "          padding: 0 2rem;" +
+                       "          float: right;" +
+                       "          border-top: 2px dotted black;" +
+                       "          min-width: 8rem;" +
+                       "      }" +
+                       "   </style> " +
+                       "</head>" +
+                       "<body>" +
+                       "   <h1>Munkalap<br>" +
+                      $"   <span class=\"alcim\">Azonosító: {work.Id}</span></h1>" +
+                       "   <fieldset>" +
+                       "       <legend>Bejelentő</legend>" +
+                       "       <p>" +
+                       "           <span class=\"label\">Neve:</span>" +
+                      $"           {work.RequesterName}" +
+                       "       </p>" +
+                       "       <p>" +
+                       "           <span class=\"label\">Időpont:</span>" +
+                      $"           {work.RequestDate:yyyy.MM.dd hh:mm}" +
+                       "       </p>" +
+                       "       <p>" +
+                       "           <span class=\"label\">Probléma:</span>" +
+                      $"           {work.Description}" +
+                       "       </p>" +
+                       "   </fieldset>" +
+                       "   <fieldset>" +
+                       "       <legend>Feladat kiosztása</legend>" +
+                       "       <p>" +
+                       "           <span class=\"label\">Felelős:</span>" +
+                      $"           {work.EmployeeName}" +
+                       "       </p>" +
+                       "       <p>" +
+                       "           <span class=\"label\">Határidő:</span>" +
+                      $"           {work.DeadLine:yyyy.MM.dd}" +
+                       "       </p>" +
+                       "       <p>" +
+                       "           <span class=\"label\">Megjegyzés:</span>" +
+                      $"           {work.AssignDetails}" +
+                       "       </p>" +
+                       "       <p class=\"alairas\">" +
+                      $"           {work.AssignerName}" +
+                       "       </p>" +
+                       "   </fieldset>" +
+                       "   <fieldset>" +
+                       "       <legend>Feladat elvégzése</legend>" +
+                       "       <p>" +
+                       "           <span class=\"label\">Dátum:</span>" +
+                      $"           {work.FinishDate:yyyy.MM.dd hh:mm}" +
+                       "       </p>" +
+                       "       <p style=\"min-height: 5rem;\">" +
+                       "           <span class=\"label\">Megjegyzés:</span>" +
+                      $"           {work.FinishComment}" +
+                       "       </p>" +
+                       "       <p class=\"alairas\">" +
+                      $"           {work.EmployeeName}" +
+                       "       </p>" +
+                       "   </fieldset>" +
+                       "   <fieldset>" +
+                       "       <legend>Ellenőrzés</legend>" +
+                       "       <p>" +
+                       "           <span class=\"label\">Dátum:</span>" +
+                      $"           {work.CheckDate:yyyy.MM.dd hh:mm}" +
+                       "       </p>" +
+                       "       <p style=\"min-height: 5rem;\">" +
+                       "           <span class=\"label\">Megjegyzés:</span>" +
+                      $"           {work.CheckComment}" +
+                       "       </p>" +
+                       "       <p class=\"alairas\" >" +
+                      $"           &nbsp;{work.CheckerUser}&nbsp;" +
+                       "       </p>" +
+                       "   </fieldset>" +
+                       "</body>";
+
+            var converter = new SelectPdf.HtmlToPdf();
+            converter.Options.PdfPageSize = SelectPdf.PdfPageSize.A4;
+            converter.Options.PdfPageOrientation = SelectPdf.PdfPageOrientation.Portrait;
+            var doc = converter.ConvertHtmlString(html);
+            return doc.Save();            
+        }
     }
 }
--- Fullstack/E-Munkalap/README.md
+++ Fullstack/E-Munkalap/README.md
@@ -1,6 +1,6 @@
 #### docker indításhoz
 ```
-curl https://raw.githubusercontent.com/BognarPal/Jedlik_FeladatMegoldasok/master/Fullstack/E-Munkalap/Dockerfile > Dockerfile
+curl https://raw.githubusercontent.com/BognarPal/Jedlik_FeladatMegoldasok/master/Fullstack/E-Munkalap/Dockerfile.fromSource > Dockerfile
 curl https://raw.githubusercontent.com/BognarPal/Jedlik_FeladatMegoldasok/master/Fullstack/E-Munkalap/docker-compose.yml > docker-compose.yml
 docker-compose build --no-cache
 docker-compose up
