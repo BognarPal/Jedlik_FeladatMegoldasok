e5b27e207e61fd536ee44c1a2eaa77a64323ce05
Bognár Pál <bognar.pal@siro.hu>
2021-03-31 19:00:41 +0200

    ADFGVX-rejtjel

---
+++ Érettségi feladatsorok/ADFGVX/ADFGVX/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ADFGVX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. feladat:");
+            Console.Write("\tKérem a kulcsot [HOLD]:");
+            string kulcs = Console.ReadLine().ToUpper();
+            if (kulcs == "")
+                kulcs = "HOLD";
+            Console.Write("\tKérem az üzenetet [szeretem a csokit]:");
+            string uzenet = Console.ReadLine().ToLower();
+            if (uzenet == "")
+                uzenet = "szeretem a csokit";
+
+
+            ADFGVXrejtjel rejtjel = new ADFGVXrejtjel("kodtabla.txt", uzenet, kulcs);
+            Console.WriteLine("5. feladat: az átalakított üzenet: {0}", rejtjel.AtalakitottUzenet());
+
+            Console.WriteLine("6. feladat s->{0} x->{1}", rejtjel.Betupar('s'), rejtjel.Betupar('x'));
+
+            Console.WriteLine("7. feladat: A kódszöveg: {0}", rejtjel.Kodszoveg());
+
+            Console.WriteLine("8. feladat: A kódolt üzenet: {0}", rejtjel.KodoltUzenet());
+        }
+    }
+}
