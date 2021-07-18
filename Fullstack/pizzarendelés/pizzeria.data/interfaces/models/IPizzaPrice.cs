568abea25a7d3d2423296cc9931354001ae457df
Bognár Pál <bognar.pal@outlook.hu>
2021-07-18 12:58:43 +0200

    korrekció #3

--- Fullstack/pizzarendelés/pizzeria.data/interfaces/models/IPizzaPrice.cs
+++ Fullstack/pizzarendelés/pizzeria.data/interfaces/models/IPizzaPrice.cs
@@ -4,7 +4,6 @@ namespace pizzeria.data.interfaces.models
 {
     public interface IPizzaPrice
     {
-        int Id { get; set; }
         DateTime FromDate { get; set; }
         DateTime? ToDate { get; set; }
         decimal Price { get; set; }
