c1ed26ab6ec7ce10b6bed0c33f3376da1f03b385
Bognár Pál <bognar.pal@outlook.hu>
2021-07-19 10:17:09 +0200

    IEmployee modell

---
+++ Fullstack/pizzarendelés/pizzeria.data/interfaces/models/IEmployee.cs
@@ -0,0 +1,10 @@
+﻿namespace pizzeria.data.interfaces.models
+{
+    public interface IEmployee : IEntity
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string PasswordHash { get; set; }
+        string Phone { get; set; }
+    }
+}
