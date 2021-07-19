9f37bdd70c53b71d6bdd88bd24c1b9537afa6930
Bognár Pál <bognar.pal@outlook.hu>
2021-07-19 09:07:08 +0200

    IOrder interface

---
+++ Fullstack/pizzarendelés/pizzeria.data/interfaces/models/IOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pizzeria.data.interfaces.models
+{
+    public interface IOrder : IEntity
+    {
+        ICustomer Customer { get; set; }
+        DateTime OrderTimeStamp { get; set; }
+        DateTime? CookingStartTimeStamp { get; set; }
+        DateTime? DeliveringTimeStamp { get; set; }
+        DateTime? DeliveredTimeStamp { get; set; }
+        IDictionary<IPizza, int> Pizzas { get; set; }
+        string CustomerComment { get; set; }
+        IAddress Address { get; set; }
+    }
+}
